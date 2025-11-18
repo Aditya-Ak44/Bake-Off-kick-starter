ac1c24a3301433bf3668df407bb1e436d615d276
Sai Aditya <95907651+Aditya-Ak44@users.noreply.github.com>
2025-11-18 22:59:19 +0000

    feat: Add path alias resolution in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Bake-Off-kick-starter/', // Replace with your repo name
+  base: '/Bake-Off-kick-starter/',
+  resolve: {
+    alias: {
+      "@": path.resolve("root", "./src"),
+    },
+  }, // Replace with your repo name
 })
